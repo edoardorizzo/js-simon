b9d8473cb94e0c94df3bad8f356b062da2136f26
edoardorizzo <edo96.rizzo@gmail.com>
2023-03-21 15:20:53 +0100

    add number to screen

--- assets/main.js
+++ assets/main.js
@@ -1,1 +1,17 @@
-alert('Hello')
+/*
+Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+*/
+
+//generare 5 numeri casuali e associarli ad un elemento in pagina
+const ul = document.createElement('ul');
+
+for (let index = 0; index < 5; index++) {
+    const li = document.createElement('li');
+    let randomNumbers = numbers[index];
+    randomNumbers = Math.floor(Math.random() * 100 + 1);
+    li.textContent = randomNumbers;
+    ul.appendChild(li)
+    console.log(randomNumbers);
+}
